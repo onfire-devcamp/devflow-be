10ba17f268a0d193bc23e3e09c4c33fc18bcaed0
huy.tranminhcs0707@hcmut.edu.vn <huy.tranminhcs0707@hcmut.edu.vn>
2026-05-23 12:46:41 +0700

    chore: set up cors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express from "express";
 import type { Request, Response } from "express";
 import "dotenv/config";
 import userRoute from "./routes/userRoute.ts";
-
+import cors from "cors";
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.get("/", async (req: Request, res: Response) => {
   res.send("API is working");
