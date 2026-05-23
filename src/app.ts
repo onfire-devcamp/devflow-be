af33cbdbee8356041805cfde1419668acfb97813
huytranminhcs0707-lab <huy.tranminhcs0707@hcmut.edu.vn>
2026-05-23 18:41:23 +0700

    Chore/setup cors (#7)
    * feat: apply jwt mechanism
    
    * feat: add middleware to verify token
    
    * fix: move the logic hash password to service layer and pull code from the main
    
    * fix: fix all comment
    
    * fix: change the hardcoded code to variable in env file
    
    * chore: set up cors
    
    ---------
    
    Co-authored-by: dhp <dohuuphuoc2303@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import express from "express";
 import type { Request, Response } from "express";
 import "dotenv/config";
 import userRoute from "./routes/userRoute.ts";
+import cors from "cors";
 import activityRoute from "./routes/activityRoute.ts";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.get("/", async (req: Request, res: Response) => {
   res.send("API is working");
