36f00ef9ffed9bdaf2147f64f74726cd6a262797
Nhu Phan <s4117555@rmit.edu.vn>
2026-05-24 19:35:43 +0700

    build(dist): compile server and streakService

--- dist/server.js
+++ dist/server.js
@@ -2,8 +2,10 @@ import "dotenv/config";
 import connectDB from "./config/database.js";
 import app from "./app.js";
 import { env } from "./config/environment.js";
+import { startStreakCron } from "./services/streakService.js";
 const port = env.PORT;
 connectDB();
+startStreakCron();
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
