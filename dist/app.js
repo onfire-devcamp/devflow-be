ae0fa2092a35a4c4203b248282cd1e5ae3708524
Nhu Phan <s4117555@rmit.edu.vn>
2026-05-24 19:34:53 +0700

    feat(security): harden app with helmet, CORS origin, body limit

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import "dotenv/config";
-import userRoute from "./routes/userRoute.js";
+import helmet from "helmet";
 import cors from "cors";
+import userRoute from "./routes/userRoute.js";
 import activityRoute from "./routes/activityRoute.js";
+import { env } from "./config/environment.js";
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.get("/", async (req, res) => {
+app.use(helmet());
+app.use(cors({
+    origin: env.CORS_ORIGIN,
+    credentials: true,
+}));
+app.use(express.json({ limit: "10kb" }));
+app.get("/", (_req, res) => {
     res.send("API is working");
 });
 app.use("/api/user", userRoute);
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 import express from "express";
 import type { Request, Response } from "express";
 import "dotenv/config";
-import userRoute from "./routes/userRoute.ts";
+import helmet from "helmet";
 import cors from "cors";
+import userRoute from "./routes/userRoute.ts";
 import activityRoute from "./routes/activityRoute.ts";
+import { env } from "./config/environment.js";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.get("/", async (req: Request, res: Response) => {
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "10kb" }));
+
+app.get("/", (_req: Request, res: Response) => {
   res.send("API is working");
 });
 
